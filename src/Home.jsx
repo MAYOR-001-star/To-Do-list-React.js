7f01e14e68606ba8324a6d320982c953d3390000
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2024-08-18 22:34:39 +0100

    edited something in the project.

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import Todo from './Todo';
 const Home = () => {
     return (
         <>
-            <div className='timmana-regular p-4 bg-[hsl(223,65%,52%)] max-w-[80rem] mx-auto border-b-2 border-[#3461d4]'>
+            <div className='timmana-regular p-4 bg-[hsl(223,65%,52%)] max-w-[55rem] mx-auto border-b-2 border-[#3461d4]'>
                 <header className='md:flex justify-around items-center bg-[#a1affa] p-3'>
                     <div className='flex justify-between items-center'>
                         <a href='/' className='text-white text-2xl font-bold flex items-center'>
