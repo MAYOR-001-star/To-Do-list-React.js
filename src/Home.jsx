f091fc8651fe03fe2dc1b624f7193e3ae6492b90
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2024-08-18 22:23:16 +0100

    Done with the whole project.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,7 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import Add_task from './Add_task';
-import CompletedTask from './CompletedTask';
-import { useGlobalContext } from './Context';
+import React from 'react';
 import Todo from './Todo';
 
 const Home = () => {
-    const [show, setShow] = useState(false);
-    const { currentView, showTodo, showCompleted, showAddTask } = useGlobalContext();
     return (
         <>
             <div className='timmana-regular p-4 bg-[hsl(223,65%,52%)] max-w-[80rem] mx-auto border-b-2 border-[#3461d4]'>
@@ -17,41 +11,10 @@ const Home = () => {
                             <img src='/images/planner-icon.png' alt='website-logo' className='w-[3rem]' />
                             <h1 className='ml-2 mt-4'>Todo Planner</h1>
                         </a>
-                        <button
-                            className='mr-4 text-white md:hidden hover:text-[#27378c]'
-                            onClick={() => setShow(!show)}
-                            aria-label={show ? 'Close menu' : 'Open menu'}
-                            aria-expanded={show}
-                        >
-                            {show ? <FaTimes /> : <FaBars />}
-                        </button>
                     </div>
-
-                    <nav>
-                        {/* <ul className={`md:flex md:space-x-4 ${show ? 'flex flex-col items-center slide-down' : 'hidden slide-up'}`}> */}
-                        <ul className={`md:flex md:space-x-4 ${show ? 'flex flex-col items-center slide-down' : 'hidden slide-up'}`}><li className='mt-2 md:mt-0'><button
-                                    className='block px-4 py-2 text-white hover:text-[#27378c] hover:border-b-2 border-[#1f3cd4]'
-                                    onClick={showAddTask}>
-                                    Home
-                                </button>
-                            </li>
-                            <li className='mt-2 md:mt-0'>
-                                <button className='block px-4 py-2 text-white hover:text-[#27378c] hover:border-b-2 border-[#1f3cd4]' onClick={showTodo}>
-                                    Todo Tasks
-                                </button>
-                            </li>
-                            <li className='mt-2 md:mt-0'>
-                                <button className='block px-4 py-2 text-white hover:text-[#27378c] hover:border-b-2 border-[#1f3cd4]' onClick={showCompleted}>
-                                    Completed Tasks
-                                </button>
-                            </li>
-                        </ul>
-                    </nav>
                 </header>
             </div>
-            {currentView === 'addTask' && <Add_task />}
-            {currentView === 'todo' && <Todo />}
-            {currentView === 'completed' && <CompletedTask />}
+            <Todo/>
         </>
     );
 };
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,67 +1,94 @@
-import React, { createContext, useState } from 'react'
-
-export const taskContext = createContext()
+import React, { useState } from 'react'
 
 const Todo = () => {
-    const [completeGlobals, setCompleteGlobals] = useState([])
+    const[todos, setTodos] = useState([])
     const[task, setTask] = useState('')
-    const[todos, setTodo] = useState([])
-    const completedTask = (id) => {
-        const filterCompletedTask = todos.find((todo)=>todo.id === id)
-        setCompleteGlobals((complete)=>{
-            const newlyCompleted = {filterCompletedTask, id:{id}}
-            return [...complete, newlyCompleted ]
-        })
-        console.log(filterCompletedTask)
-        taskRemover(id)
-    }
+    const [completedItems, setCompletedItems] = useState([])
+    const [editId, setEditId] = useState(null)
+    const [isEditing, setIsEditing] = useState(false)
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        if(task){
-            const taskInput ={id:new Date().getTime().toString(), task} 
-            setTodo((todos)=>{
-                return [...todos, taskInput]
-            })
-            setTask('')
-        }else{
-            alert('please put something in the input box!!!!') 
+    const editor = (id) => {
+        const editable = todos.find((todo)=>todo.id === id)
+        if(editable){
+            setIsEditing(true)
+            setTask(editable.task)
+            setEditId(id)
         }
     }
 
-    const taskRemover = (id) => {
-        const idRemover = todos.filter((todo)=> todo.id !== id)
-        setTodo(idRemover)
+    const submitHandler = (e) => {
+        e.preventDefault() 
+        if(task){
+            if(isEditing){
+                const updater = todos.map((todo)=>todo.id === editId ? {...todo, task} : todo)
+                setTodos(updater)
+                setIsEditing(false)
+                setEditId(null)
+            }else{
+                const newTask = {task, id: new Date().getTime().toString()}
+                setTodos([...todos, newTask])
+            }
+            setTask('')
+        }else{
+            alert('please enter a task!!!')
+        }
     }
+
+    const eliminator = (id) => {
+        const deleteItems = todos.filter((todo)=>todo.id !== id)
+        setTodos(deleteItems)
+    }
+
+    const eliminator1 = (id) => {
+        const deleteItems = completedItems.filter((item)=>item.id !== id)
+        setCompletedItems(deleteItems)
+    }
+
+    const completedTask = (id) => {
+        if(completedItems){
+            const completedTask = todos.find((todo)=>todo.id === id)
+            setCompletedItems([...completedItems, completedTask])  
+        }
+        eliminator(id)
+    }
+    
     return (
-        <div className='timmana-regular'>
-            <form onSubmit={submitHandler}>
-                <input type="text" value={task} id='task' name='task' placeholder="Enter your task" onChange={(e)=>setTask(e.target.value)}/>
-                <button type="submit">Add Todo</button>
-            </form>
-            {todos.map((todo)=>{
-                const{id, task} = todo;
-                return(
-                    <div key={id}>
-                        <p>{task}</p>
-                        <button onClick={()=>taskRemover(id)}>Delete</button>
-                        <button>Edit</button>
-                        <button onClick={()=>completedTask(id)}>Completed</button>
-                    </div>
-                )
-            })}
-            <taskContext.Provider value={{completeGlobals}}>
-                {completeGlobals.map((completed)=>{
-                    const{ id, task} = completed;
-                    return(
-                        <div key={id}>
-                            <p>{task}</p>
-                        </div>
-                    )
-                })}
-            </taskContext.Provider>
+        <div>
+            <div className='flex flex-row gap-5 justify-center my-10 items-center'>
+                <input type='text' value={task} id='task' name='task' onChange={(e)=>setTask(e.target.value)} className='border-2 border-blue-900 rounded-md'/>
+                <button onClick={submitHandler} className='border-2 border-gray rounded-md px-2 hover:bg-blue-800 hover:text-white'>{isEditing? 'Edit Task' : 'Add Task' }</button>
+            </div>
+            <div>
+                <div className='flex justify-center flex-col items-center'>
+                    <h1 className='text-2xl mb-4'>Tasks:</h1>
+                    {todos.map((todo)=>{
+                        const{id, task} = todo
+                        return(
+                            <div key={id}>
+                                <p className='mb-4 text-2xl capitalize'>{task}</p>
+                                <button onClick={()=>eliminator(id)} className='border-2 border-gray rounded-md px-2 mr-3 hover:bg-red-500 hover:text-white'>Delete</button>
+                                <button onClick={()=>editor(id)} className='border-2 border-gray rounded-md px-2 mr-3 hover:bg-gray-500 hover:text-white'>Edit</button>
+                                <button onClick={()=>completedTask(id)} className='border-2 border-gray rounded-md px-2 mr-3 hover:bg-green-400 hover:text-white'>Completed</button>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='flex justify-center flex-col items-center'>
+                    <h2 className='text-2xl mt-4'>Completed Tasks:</h2>
+                    {completedItems.map((item)=>{
+                        const{id, task} = item
+                        return(
+                            <div key={id} className='flex gap-5 mt-4 items-center'>
+                                <p className='text-xl capitalize'>{task}</p>
+                                <button onClick={()=>eliminator1(id)} className='border-2 border-gray rounded-md px-2 mr-3 hover:bg-red-500 hover:text-white'>Delete</button>
+                            </div>
+                        )
+                    })}
+                </div>
+        </div>
         </div>
     )
 }
+
 
 export default Todo
